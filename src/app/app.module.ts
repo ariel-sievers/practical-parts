b3cdbce0b956279b25b9a33a1c55a8917bfaa3ed
Ariel Sievers <booboo111998@hotmail.com>
2020-02-09 17:15:15 -0600

    Fix routing issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
 import { ProductsComponent } from './pages/products/products.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
-=======
 import { NavbarComponent } from './components/header/navbar/navbar.component';
->>>>>>> Add navbar component to header
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-<<<<<<< HEAD
     ProductsComponent,
     AboutComponent,
     ContactComponent,
-    HomeComponent
-=======
+    HomeComponent,
     NavbarComponent
->>>>>>> Add navbar component to header
   ],
   imports: [
     BrowserModule,
