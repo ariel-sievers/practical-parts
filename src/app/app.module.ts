cf489b79b95c27705052be3209a0f21e5be4ae69
Ariel Sievers <booboo111998@hotmail.com>
2020-02-09 16:46:49 -0600

    Add navbar component to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+<<<<<<< HEAD
 import { ProductsComponent } from './pages/products/products.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
+=======
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+>>>>>>> Add navbar component to header
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+<<<<<<< HEAD
     ProductsComponent,
     AboutComponent,
     ContactComponent,
     HomeComponent
+=======
+    NavbarComponent
+>>>>>>> Add navbar component to header
   ],
   imports: [
     BrowserModule,
