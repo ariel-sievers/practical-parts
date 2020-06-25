406d5e79ae28a8cb5b8e0a4d4b9052e2366aa7ef
Ariel Sievers <booboo111998@hotmail.com>
2020-06-25 11:39:13 -0500

    environemnt variable test

--- src/app/services/shop.service.ts
+++ src/app/services/shop.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-//import { environment } from 'src/environments/environment';
-import { environment } from 'src/environments/environment.prod'
+import { environment } from 'src/environments/environment';
+//import { environment } from 'src/environments/environment.prod'
 import { Observable } from 'rxjs';
 import { map, publishReplay, refCount } from 'rxjs/operators';
 
