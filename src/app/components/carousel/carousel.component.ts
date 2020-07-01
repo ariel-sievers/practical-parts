00fc0cc623ce05072ff600c81cfbd3bb802c1220
Ariel Sievers <booboo111998@hotmail.com>
2020-07-01 16:57:09 -0500

    change slides by clicking a dot representing its position and add visuals to entire carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 
+export interface Slide {
+  src: string;
+  description?: string;
+  link?: string;
+}
+
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.sass']
 })
 export class CarouselComponent implements OnInit, AfterViewInit {
-  @Input() slides;
-  currentSlide:     number;                
-  imageWidth:       number;
-  carousel:         HTMLElement;
-  carouselSlides:   NodeListOf<HTMLElement>;
+
+  // slide variables
+  @Input() slides:     Slide[];
+  currentSlide:        number;
+  currentDescription:  string;
+  currentLink:         string;                
+  imageWidth:          number;
+
+  // HTML elements
+  carousel:            HTMLElement;
+  carouselSlides:      NodeListOf<HTMLElement>;
+
+  // timer
+  slideshowTimer:      NodeJS.Timer;
 
   ngOnInit(): void {
-    this.currentSlide = 1;
+    this.currentSlide       = 1;
+    this.currentDescription = this.getSlideDescription(this.currentSlide);
+    this.currentLink        = this.getSlideLink(this.currentSlide);
+    this.slideshowTimer     = setInterval( () => { this.onNext(); }, 7000);
   }
 
   ngAfterViewInit(): void {
@@ -35,13 +53,47 @@ export class CarouselComponent implements OnInit, AfterViewInit {
   }
 
   /**
+   * Get a slide's description according to its index.
+   * @param slideIndex index of the slide to get a description from
+   */
+  getSlideDescription(slideIndex: number): string {
+    if (slideIndex <= 0 || slideIndex > this.slides.length) { return; }
+
+    let description = this.slides[slideIndex - 1].description;
+
+    // shorten description if its length is greater than 44 characters
+    if (description && description.length > 44) { description = description.substr(0, 44); }
+
+    console.log("DESCRIPTION FOUND: " + (description ? description : "none"));
+
+    return description ? description : "";
+  }
+
+  /**
+   * Get a slide's linkn according to its index.
+   * @param slideIndex index of the slide to get a link from
+   */
+  getSlideLink(slideIndex: number): string {
+    if (slideIndex <= 0 || slideIndex > this.slides.length) { return; }
+
+    const link = this.slides[slideIndex - 1].link
+    console.log("LINK FOUND: " + (link ? link : "none"));
+
+    return link ? link : ""
+  }
+
+  /**
    * Go to the previous slide.
    */
   onPrev(): void {
     if (this.currentSlide <= 0) { return; }
-    this.carousel.style.transition = 'transform 0.4s ease-in-out';
+    this.carousel.style.transition = 'transform 0.5s ease-in-out';
     this.currentSlide--;
+    this.currentDescription        = this.getSlideDescription(this.currentSlide);
+    this.currentLink               = this.getSlideLink(this.currentSlide);
     this.carousel.style.transform  = `translateX(${ this.currentSlide * -this.imageWidth }px)`;
+
+    this.restartTimer();
   }
 
   /**
@@ -49,9 +101,28 @@ export class CarouselComponent implements OnInit, AfterViewInit {
    */
   onNext(): void {
     if (this.currentSlide >= this.carouselSlides.length - 1) { return; }
-    this.carousel.style.transition = 'transform 0.4s ease-in-out';
+    this.carousel.style.transition = 'transform 0.5s ease-in-out';
     this.currentSlide++;
+    this.currentDescription        = this.getSlideDescription(this.currentSlide);
+    this.currentLink               = this.getSlideLink(this.currentSlide);
     this.carousel.style.transform  = `translateX(${ this.currentSlide * -this.imageWidth }px)`;
+
+    this.restartTimer();
+  }
+
+  /**
+   * Changes the current slide according to a specified index number.
+   * @param slideIndex index of the slide to go to
+   */
+  goToSlide(slideIndex: number): void {
+    if (slideIndex <= 0 || slideIndex >= this.carouselSlides.length - 1) { return; }
+    this.carousel.style.transition = 'transform 0.5s ease-in-out';
+    this.currentSlide              = slideIndex;
+    this.currentDescription        = this.getSlideDescription(this.currentSlide);
+    this.currentLink               = this.getSlideLink(this.currentSlide);
+    this.carousel.style.transform  = `translateX(${ this.currentSlide * -this.imageWidth }px)`;
+
+    this.restartTimer();
   }
 
   /**
@@ -60,6 +131,8 @@ export class CarouselComponent implements OnInit, AfterViewInit {
   resetToStart(): void {
     this.carousel.style.transition = 'none';
     this.currentSlide              = this.carouselSlides.length - this.currentSlide;
+    this.currentDescription        = this.getSlideDescription(this.currentSlide);
+    this.currentLink               = this.getSlideLink(this.currentSlide);
     this.carousel.style.transform  = `translateX(${ this.currentSlide * -this.imageWidth }px)`;
   }
 
@@ -69,9 +142,17 @@ export class CarouselComponent implements OnInit, AfterViewInit {
   resetToEnd(): void {
     this.carousel.style.transition = 'none';
     this.currentSlide              = this.carouselSlides.length - 2;
+    this.currentDescription        = this.getSlideDescription(this.currentSlide);
+    this.currentLink               = this.getSlideLink(this.currentSlide);
     this.carousel.style.transform  = `translateX(${ this.currentSlide * -this.imageWidth }px)`;
   }
 
-  
+  /**
+   * Restart the interval timer which automates the slideshow.
+   */
+  restartTimer(): void {
+    clearInterval(this.slideshowTimer);
+    this.slideshowTimer = setInterval( () => { this.onNext() }, 7000);
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ShopService } from 'src/app/services/shop.service';
+import { Slide } from 'src/app/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +9,11 @@ import { ShopService } from 'src/app/services/shop.service';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
-  slides = [
-    { src: "https://i.imgur.com/5mXfwr1.jpg" },
-    { src: "https://www.atomix.com.au/media/2017/07/StockPhotoBanner.jpg" },
+  slides: Slide[] = [
+    { src: "https://i.imgur.com/5mXfwr1.jpg", description: "Introducing our new product, the clamp thing" },
+    { src: "https://www.atomix.com.au/media/2017/07/StockPhotoBanner.jpg", description: "Click here to learn more about us", link: "/about" },
     { src: "https://www.liveabout.com/thmb/s6wcupyhjrstpTmxv_3K8kLIPYM=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/feet-face-599c412c22fa3a0011d92add.jpg" },
-    { src: "https://assets.classicfm.com/2019/11/superman-violin-1553266007-view-0.jpg" }
+    { src: "https://assets.classicfm.com/2019/11/superman-violin-1553266007-view-0.jpg", description: "Sleep well knowing you saved $$$" }
   ]
 
   constructor(public shopService: ShopService, public router: Router) {}
